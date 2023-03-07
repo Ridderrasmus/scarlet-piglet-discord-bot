0fbb15ea5a496fb32dc2240811c9e1ef7d1bdfef
Rasmus Tanggaard <rasmus@tthost.dk>
2023-03-07 11:08:59 +0100

    Small fix

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from discordbot import *
 import multiprocessing
 import os
+import utils
+
+log = utils.log_handler
 
 def main():
     # Run the bot
-    BOT.run(token=os.getenv('DISCORD_TOKEN'), log_handler=log_handler)
+    BOT.run(token=os.getenv('DISCORD_TOKEN'), log_handler=log)
     
 
 if __name__ == "__main__":
